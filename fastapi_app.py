6115003d2a6991b2ce9df0abed2e20a8c57ebf1e
Fariz <fariz.nowzin7@gmail.com>
2023-08-19 22:19:56 +0530

    Added min and max value

--- fastapi_app.py
+++ fastapi_app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 from pydantic import BaseModel
 from openai_integration import generate_response
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
@@ -21,9 +26,17 @@ class HeartAttackPredictionRequest(BaseModel):
 
 @app.post("/predict")
 def predict_heart_attack(data: HeartAttackPredictionRequest):
-    prompt = f"Predict the probability of a heart attack with the following input:\n\nAge: {data.age}\nSex: {data.sex}\nChest Pain Type: {data.cp}\nTrestbps: {data.trestbps}\nChol: {data.chol}\nFBS: {data.fbs}\nRestecg: {data.restecg}\nThalach: {data.thalach}\nExang: {data.exang}\nOldpeak: {data.oldpeak}\nSlope: {data.slope}\nCA: {data.ca}\nThal: {data.thal}\n\nRisk factors for heart attack are: smoking, high blood pressure, high cholesterol, obesity, diabetes, sedentary lifestyle, and family history."
+    try:
+        prompt = f"Predict the probability of a heart attack with the following input:\n\nAge: {data.age}\nSex: {data.sex}\nChest Pain Type: {data.cp}\nTrestbps: {data.trestbps}\nChol: {data.chol}\nFBS: {data.fbs}\nRestecg: {data.restecg}\nThalach: {data.thalach}\nExang: {data.exang}\nOldpeak: {data.oldpeak}\nSlope: {data.slope}\nCA: {data.ca}\nThal: {data.thal}\n\nRisk factors for heart attack are: smoking, high blood pressure, high cholesterol, obesity, diabetes, sedentary lifestyle, and family history."
 
-    prediction = generate_response(prompt)
+        prediction = generate_response(prompt)
+        
+        return {"prediction": prediction}
+        #return {"prediction": prediction.item()}  # Return prediction as a single value
     
-    return {"prediction": prediction}
-    #return {"prediction": prediction.item()}  # Return prediction as a single value
+    except Exception as e:
+        logger.error(f"Error occurred: {str(e)}")
+        return {"error": "Failed to process the request."}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,26 @@
+import logging
 import requests
 import streamlit as st
 
 API_URL = "http://localhost:8000"  # Replace with your FastAPI server URL
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 st.title("Heart Attack Prediction")
 
 age = st.number_input("Age", min_value=1, max_value=100, value=30)
 sex = st.selectbox("Sex", ["Male", "Female"])
-cp = st.selectbox("Chest Pain Type", [0, 1, 2, 3])
-trestbps = st.number_input("Resting Blood Pressure")
-chol = st.number_input("Cholesterol")
-fbs = st.selectbox("Fasting Blood Sugar", [0, 1])
-restecg = st.selectbox("Resting Electrocardiographic Results", [0, 1, 2])
-thalach = st.number_input("Maximum Heart Rate Achieved")
-exang = st.selectbox("Exercise Induced Angina", [0, 1])
-oldpeak = st.number_input("ST Depression Induced by Exercise")
+cp = st.selectbox("Chest Pain Type", [0, 1, 2, 3]) ##  0 = Typical Angina, 1 = Atypical Angina, 2 = Non-anginal Pain, 3 = Asymptomatic
+trestbps = st.number_input("Resting Blood Pressure [mm Hg]", min_value=0, max_value=300) 
+chol = st.number_input("Cholesterol [mg/dl]", min_value=0, max_value=1000)
+fbs = st.selectbox("Fasting Blood Sugar", [0, 1]) ## fasting blood sugar > 120 mg/dl) (1 = true; 0 = false)
+restecg = st.selectbox("Resting Electrocardiographic Results", [0, 1, 2]) ## 0 = Normal, 1 = ST-T wave normality, 2 = Left ventricular hypertrophy
+thalach = st.number_input("Maximum Heart Rate Achieved", min_value=0, max_value=300)
+exang = st.selectbox("Exercise Induced Angina", [0, 1]) ## (1 = yes; 0 = no)
+oldpeak = st.number_input("ST Depression Induced by Exercise", min_value=0.0, max_value=10.0)
 slope = st.selectbox("Slope of the Peak Exercise ST Segment", [0, 1, 2])
-ca = st.number_input("Number of Major Vessels Colored by Fluoroscopy")
+ca = st.number_input("Number of Major Vessels Colored by Fluoroscopy", min_value=0, max_value=4)
 thal = st.selectbox("Thalassemia", [0, 1, 2, 3])
 
 if st.button("Predict"):
@@ -36,9 +40,18 @@ if st.button("Predict"):
         "thal": thal
     }
     
-    response = requests.post(f"{API_URL}/predict", json=data)
-    if response.status_code == 200:
-        prediction = response.json().get("prediction")
-        st.write("Prediction:", prediction)
-    else:
-        st.error("Failed to get prediction. Please try again.")
+    try:
+        response = requests.post(f"{API_URL}/predict", json=data)
+        if response.status_code == 200:
+            prediction = response.json().get("prediction")
+            st.write("Prediction:", prediction)
+        else:
+            st.error("Failed to get prediction. Please try again.")
+            logger.error(f"Prediction request failed with status code: {response.status_code}")
+            
+    except requests.exceptions.RequestException as e:
+        st.error("Failed to connect to the server. Please try again later.")
+        logger.error(f"Server connection error: {str(e)}")
+    except Exception as e:
+        st.error("An error occurred. Please try again.")
+        logger.error(f"Error occurred: {str(e)}")
