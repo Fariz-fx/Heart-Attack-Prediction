4916fff84b46b387a24561ef69e33422ac32de70
Fariz <fariz.nowzin7@gmail.com>
2023-08-23 00:28:48 +0530

    Cleanuptrainmodelfile (#4)
    * cleanedup train model file
    
    * Added last line

--- app/model/train_model.py
+++ app/model/train_model.py
@@ -1,46 +1,75 @@
 import pandas as pd
-#from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
-#from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
-#import pickle
 import joblib
+import logging
 
-# Load your data
-df = pd.read_csv("data.csv")
+# Declaration
+file_name="data.csv" # Provide File Name
+axis_value=1
+result_column="output" # In CSV the result column name
+scaler_joblib_name="scaler.joblib"
+model_joblib_name="model.joblib"
+test_size_declared=0.2 
+random_state_declared=0
 
-# 'target' should be replaced with your actual target column
-X = df.drop('output', axis=1)
-y = df['output']
+def load_data(path: str):
+    try:
+        return pd.read_csv(path)
+    except FileNotFoundError as e:
+        logging.error(f"The provided file path {path} does not exist.")
+        raise
+    except Exception as e:
+        logging.error("Unable to load data.")
+        raise
 
-# Normalize the feature data
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
+def normalize_data(raw_data):
+    try:
+        scaler = StandardScaler()
+        normalized_data = scaler.fit_transform(raw_data)
+        return normalized_data, scaler
+    except Exception as e:
+        logging.error("Unable to normalize data.")
+        raise
 
-# Split into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)#42)
+def split_data(X, y, test_size=test_size_declared, random_state=random_state_declared):
+    try:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+        return X_train, X_test, y_train, y_test
+    except Exception as e:
+        logging.error("Unable to split data.")
+        raise
 
-# Initialize classifier
-#model = RandomForestClassifier(n_estimators=100)
-#model = LogisticRegression()
-model = SVC()
+def train_save_model(X_train, y_train, model_path):
+    try:
+        model = SVC()
+        model.fit(X_train, y_train)
+        joblib.dump(model, model_path)
+        return model
+    except Exception as e:
+        logging.error("Unable to train model.")
+        raise
 
-# Train model
-model.fit(X_train, y_train)
+def test_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    return accuracy_score(y_test, y_pred)
 
-# Save the trained model and scaler
-# pickle.dump(model, open('model.pkl', 'wb'))
-# pickle.dump(scaler, open('scaler.pkl', 'wb'))
+def perform_cross_validation(model, X_train, y_train, cv=5):
+    scores = cross_val_score(estimator=model, X=X_train, y=y_train, cv=cv)
+    return scores.mean()
 
-joblib.dump(model, 'model.joblib')
-joblib.dump(scaler, 'scaler.joblib')
+def main():
+    df = load_data(file_name)
+    X = df.drop(result_column, axis=axis_value)
+    y = df[result_column]
+    X, scaler = normalize_data(X) 
+    joblib.dump(scaler, scaler_joblib_name)
+    X_train, X_test, y_train, y_test = split_data(X, y)
+    model = train_save_model(X_train, y_train, model_joblib_name)
+    print("Model Accuracy: ", test_model(model, X_test, y_test))
+    print(f"Cross Validation Score: {perform_cross_validation(model, X_train, y_train)}\n")
 
-# Get and print accuracy of the model
-y_pred = model.predict(X_test)
-print("Model Accuracy: ", accuracy_score(y_test, y_pred))
-
-# Cross validation
-scores = cross_val_score(estimator=model, X=X_train, y=y_train, cv=5)
-print(f"Cross Validation Score: {scores.mean()}\n")
+if __name__ == '__main__':
+    main()
