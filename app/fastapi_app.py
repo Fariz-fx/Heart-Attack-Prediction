14e167d24319d3e63b736e6d82fce09304d10d90
Fariz <fariz.nowzin7@gmail.com>
2023-08-29 22:24:57 +0530

    basic code

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -18,10 +18,8 @@ now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 app = FastAPI()
 
 # Load the trained model 
-# model = pickle.load(open('model/model.pkl', 'rb'))
-# scaler = pickle.load(open('model/scaler.pkl', 'rb')) 
-model = joblib.load('model/Heart_Attack_model.joblib')
-scaler = joblib.load('model/Heart_Attack_scaler.joblib') 
+model = joblib.load('model/Heart_Attack_KNN_model.joblib')
+scaler = joblib.load('model/Heart_Attack_KNN_scaler.joblib') 
 
 class HeartAttackPredictionRequest(BaseModel):
     age: int
@@ -48,22 +46,9 @@ def predict_heart_attack(data: HeartAttackPredictionRequest):
     try:
         # Logging essential information
         logger.info(f"Data Submitted: {now}")
-        # logger.info(f"Age: {data.age}")
-        # logger.info(f"Sex: {data.sex}")
-        # logger.info(f"CP: {data.cp}")
-        # logger.info(f"Trestbps: {data.trestbps}")
-        # logger.info(f"Chol: {data.chol}")
-        # logger.info(f"FBS: {data.fbs}")
-        # logger.info(f"Restecg: {data.restecg}")
-        # logger.info(f"Thalach: {data.thalach}")
-        # logger.info(f"Exang: {data.exang}")
-        # logger.info(f"Oldpeak: {data.oldpeak}")
-        # logger.info(f"Slope: {data.slope}")
-        # logger.info(f"CA: {data.ca}")
-        # logger.info(f"Thal: {data.thal}")
         prompt = f"Predict the probability of a heart attack with the following input:\n\nAge: {data.age}\nSex: {data.sex}\nChest Pain Type: {data.cp}\nTrestbps: {data.trestbps}\nChol: {data.chol}\nFBS: {data.fbs}\nRestecg: {data.restecg}\nThalach: {data.thalach}\nExang: {data.exang}\nOldpeak: {data.oldpeak}\nSlope: {data.slope}\nCA: {data.ca}\nThal: {data.thal}\n\nRisk factors for heart attack are: smoking, high blood pressure, high cholesterol, obesity, diabetes, sedentary lifestyle, and family history."
         prediction = generate_response(prompt)
-        logger.info(f"Prompting: {prediction}")
+        logger.info(f"ChatGPT Prompting: {prediction}")
         return {"prediction": prediction}
             #return {"prediction": prediction.item()}  # Return prediction as a single value
 
